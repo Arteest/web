bf3cfa0668d1c43a765465ebf90cbefa47ae97e3
Vini Dy <vinidy@gmail.com>
2014-06-09 19:10:18 -0400

    style changes to frontend

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res) {
     res.render('index', {
         id: 'index',
-        title: 'Index'
+        title: 'Arteest'
     });
 });
 
